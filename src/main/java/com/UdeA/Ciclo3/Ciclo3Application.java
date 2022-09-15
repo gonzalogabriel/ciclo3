30d131408fd4272ebd5a9014af1d19a200017436
Decoders <decoderscompany@gmail.com>
2022-09-15 18:20:03 -0500

    Primer Commit de nuevo

--- src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@SpringBootApplication
+@SpringBootApplication //(exclude = {SecurityAutoConfiguration.class})
 public class Ciclo3Application {
 
 	@GetMapping("/hello")
